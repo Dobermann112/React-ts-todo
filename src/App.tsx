a651712902bf4edcb2a6067b70eabaf55eff616e
dobermann112 <ruffy112@icloud.com>
2026-02-21 17:37:09 +0900

    タスクのフィルタリング機能の追加

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,31 @@ type Todo = {
   removed: boolean;
 };
 
+type Filter = 'all' | 'checked' | 'unchecked' | 'removed';
+
 export const App = () => {
   const [text, setText] = useState('');
   const [todos, setTodos] = useState<Todo[]>([]);
+  const [filter, setFilter] = useState<Filter>('all');
+
+  const handleFilter = (filter: Filter) => {
+    setFilter(filter);
+  };
+
+  const filteredTodos = todos.filter((todo) => {
+    switch (filter) {
+      case 'all':
+        return !todo.removed;
+      case 'checked':
+        return todo.checked && !todo.removed;
+      case 'unchecked':
+        return !todo.checked && !todo.removed;
+      case 'removed':
+        return todo.removed;
+      default:
+        return todo;
+    }
+  });
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setText(e.target.value);
@@ -70,17 +92,33 @@ export const App = () => {
 
   return (
     <div>
+      <select defaultValue="all" onChange={(e) => handleFilter(e.target.value as Filter)}>
+        <option value="all">全てのタスク</option>
+        <option value="checked">完了したタスク</option>
+        <option value="unchecked">現在のタスク</option>
+        <option value="removed">ゴミ箱</option>
+      </select>
       <form
         onSubmit={(e) => {
           e.preventDefault();
           handleSubmit();
         }}
       >
-        <input type="text" value={text} onChange={(e) => handleChange(e)} />
-        <input type="submit" value="追加" onSubmit={handleSubmit} />
+        <input
+          type="text" 
+          value={text} 
+          disabled={filter === 'checked' || filter === 'removed'}
+          onChange={(e) => handleChange(e)} 
+        />
+        <input 
+          type="submit" 
+          value="追加" 
+          disabled={filter === 'checked' || filter === 'removed'}
+          onSubmit={handleSubmit} 
+        />
       </form>
       <ul>
-        {todos.map((todo) => {
+        {filteredTodos.map((todo) => {
           return (
             <li key={todo.id}>
               <input
